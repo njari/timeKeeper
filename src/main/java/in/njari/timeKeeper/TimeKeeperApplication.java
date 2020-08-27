43780e7ce71bed861056305d4920edb7581b452f
Nubra <nklamusing@gmail.com>
2020-08-28 00:21:44 +0530

    swagger fixed

--- src/main/java/in/njari/timeKeeper/TimeKeeperApplication.java
+++ src/main/java/in/njari/timeKeeper/TimeKeeperApplication.java
@@ -3,7 +3,10 @@ package in.njari.timeKeeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class TimeKeeperApplication {
 
 	public static void main(String[] args) {
